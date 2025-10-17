d5297fdf76ea5edee2d660853b8da3bccc71923d
Eduardo Barros <eduardobarros@Eduardos-MacBook-Pro.local>
2025-10-17 21:21:37 +0400

    task: connect Postgres serverdb - heroku

--- api/db.py
+++ api/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE = os.getenv("DATABASE_URL")
+DATABASE = os.getenv("DATABASE")
 
 engine = create_engine(
     DATABASE,
